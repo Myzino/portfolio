1b1b6ba06166e02e3954e3551fe44888165255bd
Jeffrey Sedoro <jeffreysedoro5@gmail.com>
2024-10-22 22:24:48 +0800

    ........

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     <h1>Portfolio</h1>
     <Link href="https://github.com/Myzino">
-    <Button variant="outline" >My Github Account</Button>
+    <Button variant="outline" >My Github Account lol</Button>
     </Link>
     </div>
   );
