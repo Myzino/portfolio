e4e7a2fd99a35e65ee8d169aaa590599cf8a98d0
Jeffrey Sedoro <jeffreysedoro5@gmail.com>
2024-10-31 12:51:00 +0800

    asd;lauihiowduykaw

--- src/app/components/ui/header.tsx
+++ src/app/components/ui/header.tsx
@@ -9,6 +9,9 @@ export default function Header() {
             <div className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl font-bold"></h1>
                 <nav className="space-x-4">
+                <Link href="/" className="hover:underline">
+                        Home
+                    </Link>
                     <Link href="/about" className="hover:underline">
                         About Me
                     </Link>
